62c957b1807429219b5238b201ef86cd7110a212
Yves Desjardins <YvesDesjardins@gmail.com>
2019-05-17 14:05:45 -0700

    implement users

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga');
+const { prisma } = require('./generated/prisma-client');
 
 const resolvers = {
   Query: {
@@ -20,6 +21,7 @@ const resolvers = {
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: { prisma },
 });
 
 server.start(()=> console.log(`Server is running on http://localhost:4000`));
