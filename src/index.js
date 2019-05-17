e049956bef0241cdc8c9ae9887f97a5005c47209
Yves Desjardins <YvesDesjardins@gmail.com>
2019-05-17 13:52:04 -0700

    switch to prisma DB

--- src/index.js
+++ src/index.js
@@ -1,42 +1,18 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}];
-
 const resolvers = {
   Query: {
     info: () => 'This is the API of a Hackernews Clone',
-    feed: () => links,
+    feed: (root, args, context, info) => {
+      return context.prisma.links();
+    },
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${ links.length }`,
-        description: args.description,
+    post: (root, args, context) => {
+      return context.prisma.createLink({
         url: args.url,
-      };
-      links.push(link);
-
-      return link;
-    },
-    updateLink: (parent, args) => {
-      const id = links.findIndex(x => x.id === args.id);
-      links[id] = {
-        id: `link-${ id }`,
-        description: args.description ? args.description : links[id].description,
-        url: args.url ? args.url : links[id].url,
-      }
-
-      return links[id];
-    },
-    deleteLink: (parent, args) => {
-      const link = links[links.findIndex(x => x.id === args.id)];
-      links = links.filter(x => x.id !== args.id);
-
-      return link;
+        description: args.description,
+      });
     },
   }
 };
